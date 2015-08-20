db9852b1a6e80fb461018693bf7a4010eb8964d8
Ian Villanueva <roland@dottystylecreative.com>
2015-08-20 16:21:15 +0800

    added homepage structure, finish footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 
+$(function() {
+    console.log( "ready!" );
+    var _viewport = {
+    	width : $(window).width(),
+    	height : $(window).height()
+    }
+    $('.hero-container, .hero-overlay, .about, .vehicle-type, .more-intel').css({
+    	height : _viewport.height + 'px'
+    });
+
+
+
+
+
+
+
+
+
+});
