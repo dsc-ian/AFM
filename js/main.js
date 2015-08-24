6bf2441345feaea688a08bacf325b4055df1e930
Ian Villanueva <roland@dottystylecreative.com>
2015-08-24 12:35:46 +0800

    add click nav animation and countdown timer

--- js/main.js
+++ js/main.js
@@ -15,31 +15,103 @@ $(function() {
 
 
 	$('.hamburger').click(function(){
-		var _this = $(this);
+		var _this = $(this), tweenLogo, tweenBurger, tweenHeader, tweenPatty;
+
+		var patties = ['.bar-1', '.bar-2', '.bar-3'];
+
 
 		if(_this.hasClass('active')){
-			_this.removeClass('active');
-			$('.navigation').hide();
-			$('.logo').show();
+			//_this.removeClass('active');
+			
+		tweenHeader = TweenMax.to('.navigation', 1, {y:0, ease: Expo.easeIn});
+		tweenPatty = TweenMax.to('.bar-1', 1, {
+			opacity:1, 
+			y:-100, 
+			rotation:-45, 
+			ease:Expo.easeIn,
+			onComplete: (function(){
+				TweenMax.to('.bar-1', 0.1, {rotation:0});
+			})
+		});
+		tweenPatty = TweenMax.to('.bar-3', 1, {opacity:1, 
+			y:-100, 
+			rotation:45, 
+			ease:Expo.easeIn, 
+			onComplete: (function(){
+				TweenMax.to('.bar-3', 0.1, {rotation:0});
+				_this.removeClass('active');
+			})
+		});
+			
+		tweenBurger = TweenMax.staggerTo(patties.reverse(), 0.5, { 
+			opacity:1, 
+			y:0, 
+			ease:Expo.easeOut,
+			delay: 1.5
+		}, 0.1);
+
+		tweenLogo = TweenMax.to('.logo', 1, {y:'0%', ease:Expo.easeOut, delay:1.5});
+
+
+			//$('.navigation').hide();
+			//$('.logo').show();
 		} else {
-			_this.addClass('active');
-			$('.navigation').show();
-			$('.logo').hide();
+
+		
+		tweenLogo = TweenMax.to('.logo', 0.8, {y:'-100%', ease:Expo.easeIn});
+			tweenBurger = TweenMax.staggerTo(patties, 0.5, {
+				opacity:0, 
+				y:-100, 
+				ease:Expo.easeIn,
+				onComplete: (function(){
+					tweenPatty = TweenMax.to('.bar-1', 1, {opacity:1, y:0, rotation:-45, ease:Expo.easeOut, delay:0.7});
+					tweenPatty = TweenMax.to('.bar-3', 1, {opacity:1, y:0, rotation:45, ease:Expo.easeOut, delay:0.7});
+
+					_this.addClass('active');
+				})
+			}, 0.1);
+
+		tweenHeader = TweenMax.to('.navigation', 1, {y:138, ease:Expo.easeOut, delay:1});
+			//_this.addClass('active');
+
+			// element, speed, easing
+			
+
+
+			//$('.navigation').show();
+			//$('.logo').hide();
 		}
 	});
+	
+	$('.countdown').timeTo({
+	    timeTo: new Date(new Date('Mon Aug 31 2015 09:00:00 GMT+0800 (PHT)')),
+	    displayDays: 2,
+	    displayCaptions: true,
+	    fontSize: 65,
+	    captionSize: 10
+	}); 
 
+});
 
-
-
-
+$(window).resize(function(){
 
 });
 
 function imageHero() {
 	var wH = window.innerHeight ? window.innerHeight : $(window).height();
 	//jQuery('.hero-container').css('height',wH - 20);
-	$('.hero-container, .hero-overlay').css('height',wH+75);
+	$('.hero-container, .hero-overlay, .hero-image').css('height',wH+75);
 	console.log('test');
+
+//	$('.hero-container img').centerImage();
+}
+
+
+function last_child() {
+  //if ($.browser.msie && parseInt($.browser.version, 10) <= 8) {
+
+    $('*:last-child').addClass('last-child');
+  //}
 }
 
 /*
