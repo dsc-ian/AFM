62ebffcbd046964a26359cb0344020174a9da15a
Ian Villanueva <roland@dottystylecreative.com>
2015-08-26 15:38:11 +0800

    update

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ $(function() {
 		width : $(window).width(),
 		height : $(window).height()
 	}
+
     var _navSubHeight = '';
+
 	$('#home').addClass('intel-active');
 
 	if(_viewport.width <= 768) {
-		$('body').addClass('mobile');
-		//$('.navigation').css({height:_viewport.height + 'px'});	
+		$('body').addClass('mobile');	
 	} else {
 		$('body').removeClass('mobile');
 	}
@@ -27,21 +28,14 @@ $(function() {
 	changeImageMobile();
 	onLoadAnimation();
 
-	$.stellar({
-		horizontalScrolling: false,
-		verticalOffset: 40
-	});
 
-    /*
-    $('.hero-container, .hero-overlay, .about, .vehicle-type, .more-intel').css({
-    	height : _viewport.height + 'px'
-    });*/
 
 	$('.btn-close').click(function(e){
 		e.preventDefault();
 		$('#home').removeClass('intel-active');
 		//$(window).scrollTop( 0 );
 		console.log('test');
+		return false;
 	});
 
 	var navContainer = $('.main-nav');
@@ -55,10 +49,7 @@ $(function() {
 			_this.addClass('active');
 		});
 
-		TweenMax.set('.sub-item', {
-					opacity:0, 
-					y: -30
-				});
+		TweenMax.set('.sub-item', {opacity:0, y: -30 });
 
 		_item.on('mouseenter', function(){
 			var _this = $(this);
@@ -82,81 +73,78 @@ $(function() {
 					opacity:0, 
 					y: -30,
 					ease:Expo.easeIn,
-
 				},0.1);
-			//_this.children('ul').removeClass('active');
 		});
 
 
 
 	});
 
-	$('.hamburger').click(function(){
+	$('.hamburger').click(function(e){
+
+		e.preventDefault();
 		var _this = $(this), tweenLogo, tweenBurger, tweenHeader, tweenPatty;
 
 		var patties = ['.patty-1', '.patty-2', '.patty-3'];
 
-		
-		
-		
-
 		if(_this.hasClass('active')){
 			//_this.removeClass('active');
 			
-		tweenHeader = TweenMax.to('.navigation', 1, {y:0, display:'none', ease: Expo.easeIn});
-		tweenPatty = TweenMax.to('.patty-1', 1, {
-			opacity:1, 
-			y:-100, 
-			rotation:-45, 
-			ease:Expo.easeIn,
-			onComplete: (function(){
-				TweenMax.to('.patty-1', 0.1, {rotation:0});
-			})
-		});
-		tweenPatty = TweenMax.to('.patty-3', 1, {opacity:1, 
-			y:-100, 
-			rotation:45, 
-			ease:Expo.easeIn, 
-			onComplete: (function(){
-				TweenMax.to('.patty-3', 0.1, {rotation:0});
-				_this.removeClass('active');
-				$('.header').removeClass('nav-active');
-			})
-		});
-			
-		tweenBurger = TweenMax.staggerTo(patties.reverse(), 0.5, { 
-			opacity:1, 
-			y:0, 
-			ease:Expo.easeOut,
-			delay: 1.5
-		}, 0.1);
+			tweenHeader = TweenMax.to('.navigation', 1, {y:0, display:'none', ease: Expo.easeIn});
 
-		tweenLogo = TweenMax.to('.logo', 1, {y:'0%', ease:Expo.easeOut, delay:1.5});
+			tweenPatty = TweenMax.to('.patty-1', 1, {
+				opacity:1, 
+				y:-100, 
+				rotation:-45, 
+				ease:Expo.easeIn,
+				onComplete: (function(){
+					TweenMax.to('.patty-1', 0.1, {rotation:0});
+				})
+			});
 
-			scrollLock(false);
+			tweenPatty = TweenMax.to('.patty-3', 1, {opacity:1, 
+				y:-100, 
+				rotation:45, 
+				ease:Expo.easeIn, 
+				onComplete: (function(){
+					TweenMax.to('.patty-3', 0.1, {rotation:0});
+					_this.removeClass('active');
+					$('.header').removeClass('nav-active');
+				})
+			});
+				
+			tweenBurger = TweenMax.staggerTo(patties.reverse(), 0.5, { 
+				opacity:1, 
+				y:0, 
+				ease:Expo.easeOut,
+				delay: 1.5
+			}, 0.1);
+
+			tweenLogo = TweenMax.to('.logo', 1, {y:'0%', ease:Expo.easeOut, delay:1.5});
+				scrollLock(false);
 
 		} else {
 
-		
-   	 	//TweenMax.to(window, 0.1, {scrollTo:{y:100}});
+			tweenLogo = TweenMax.to('.logo', 0.8, {y:'-100%', ease:Expo.easeIn});
+				tweenBurger = TweenMax.staggerTo(patties, 0.5, {
+					opacity:0, 
+					y:-100, 
+					ease:Expo.easeIn,
+					onComplete: (function(){
+						tweenPatty = TweenMax.to('.patty-1', 1, {opacity:1, y:0, rotation:-45, ease:Expo.easeOut, delay:0.7});
+						tweenPatty = TweenMax.to('.patty-3', 1, {opacity:1, y:0, rotation:45, ease:Expo.easeOut, delay:0.7});
 
-		tweenLogo = TweenMax.to('.logo', 0.8, {y:'-100%', ease:Expo.easeIn});
-			tweenBurger = TweenMax.staggerTo(patties, 0.5, {
-				opacity:0, 
-				y:-100, 
-				ease:Expo.easeIn,
-				onComplete: (function(){
-					tweenPatty = TweenMax.to('.patty-1', 1, {opacity:1, y:0, rotation:-45, ease:Expo.easeOut, delay:0.7});
-					tweenPatty = TweenMax.to('.patty-3', 1, {opacity:1, y:0, rotation:45, ease:Expo.easeOut, delay:0.7});
+						_this.addClass('active');
+					$('.header').addClass('nav-active');
+					})
+				}, 0.1);
 
-					_this.addClass('active');
-				$('.header').addClass('nav-active');
-				})
-			}, 0.1);
-
-		tweenHeader = TweenMax.to('.navigation', 1, {y:138, display:'block', ease:Expo.easeOut, delay:1});
-			scrollLock(true);
+			tweenHeader = TweenMax.to('.navigation', 1, {y:138, display:'block', ease:Expo.easeOut, delay:1});
+				scrollLock(true);
 		}
+
+
+		return false;
 	});
 	
 	
@@ -227,49 +215,30 @@ $(window).scroll(function() {
 		height : $(window).height()
 	}
     var height = $(window).scrollTop();
-    console.log(height);
+    var heroHeight = $('.hero-overlay2').scrollTop();
 
     if($('#home').hasClass('intel-active')) {
 	   
     	if(_viewport.width > 600) {
 		    if(height == 101 ) {
-		        // do something
-		       	//$('html, body').animate({ scrollTop: 0 }, 1000, 'easeOutExpo');
-		        //$('#home').removeClass('intel-active');
-		        //$(this).scrollTop(0);
-   	 			//return false;	
-   	 			TweenMax.to(window, 0.5, {scrollTo:{y:100}, ease:Ease.easeOut});
-   	 			//$('html, body').stop().animate({ scrollTop: 100 }, 500, 'easein');
-		    } 
-
-		
-
+		  		TweenMax.to(window, 0.5, {scrollTo:{y:100}, ease:Ease.easeOut});
+   	 	    } 
 
 		    if(height > 100 ) {
 		    	$('#home').addClass('scrolling')
    	 			$('#home').addClass('intel-hide');
+   	 			$('.hero-image').addClass('scroll-y');
 		    } else {
    	 			$('#home').removeClass('intel-hide');
+   	 			$('.hero-image').removeClass('scroll-y');
 		    }
-		    
 		
-		    	
-
 		} else {
 		    if(height == 61 ) {
-			       // do something
-		        //$('html, body').animate({ scrollTop: 0 }, 1000, 'easeOutExpo');
-		        //$('#home').removeClass('intel-active');
-		        //$(this).scrollTop(0);
-    			//return false;
-    			TweenMax.to(window, 0.5, {scrollTo:{y:60}, ease:Ease.easeOut});
-    			//$('#home').removeClass('intel-active');
-   	 			//$('html, body').stop().animate({ scrollTop: 50 }, 500, 'easein');
-		    } 
-
-		    
-		  		
-		    if(height > 60) {
+			  	TweenMax.to(window, 0.5, {scrollTo:{y:60}, ease:Ease.easeOut});
+    		 } 
+	
+		    if(height > 59) {
 		    	$('#home').addClass('scrolling')
    	 			$('#home').addClass('intel-hide');
 		    } else {
@@ -313,50 +282,13 @@ function imageHero() {
 		_additionHeight = 75+100;
 	}
 	console.log(_additionHeight);
-	$('.hero-overlay, .hero-image, .hero-container').css({
+	$('.hero-overlay, .hero-image, .hero-container, .hero-overlay2').css({
 		'height': wH+_additionHeight
 	});
 	//onLoadAnimation();
 	console.log(wW);
 
 }
-/*
-	var container = $(".hero-container"),
-	overlay = $(".hero-overlay"),
-	hero = $(".hero-image"),
-	btn1 = $(".logo"),
-
-
-	tn2 = new TweenMax.to(container, 1, {opacity:0, top:wH, left:wW});
-
-
-
-	TweenMax.set(container, {width:wW, height:wH, opacity:1, top:0, left:0});
-	btn1.click(function(e) {
-		e.preventDefault();
-		tn2.play(0);
-		console.log('booom');
-	});*/
-	/*
-
-    tn2 = new TweenMax.to(container, 1, {clip:'rect(0, 0, '+wH+'px, 0)', paused:true, repeat:1, yoyo:true}),
-	tn1 = new TweenMax.to(container, 2, {left:0, paused:true});
-
-	TweenMax.set(container, {
-		rotation:11, 
-		left:0, 
-		top:0, 
-		width:wW, 
-		height:wH, 
-		clip: 'rect(0px, '+wW+'px, '+wH+'px, 0px)'
-	});
-	TweenMax.set(overlay, {rotation:-11, top:0, left:0, width:wW, height:wH});
-	TweenMax.set(hero, {width:wW, height:wH});
-*/
-
-//	$('.hero-container img').centerImage();
-
-
 
 function last_child() {
   //if ($.browser.msie && parseInt($.browser.version, 10) <= 8) {
@@ -375,7 +307,6 @@ function scrollLock(active) {
 		}
 	}
 }
-
 
 function getRelativeDate(days, hours, minutes){
     var date = new Date((new Date()).getTime() + 60000 /* milisec */ * 60 /* minutes */ * 24 /* hours */ * days /* days */);
